713b3c5d48cbe3847f6581c92a8b2bba06c7a8f4
SeniorWalnut <agafonov@etm-system.ru>
2019-04-18 22:33:41 +0200

    filters - done

--- components/Catalogue.js
+++ components/Catalogue.js
@@ -3,60 +3,102 @@ import axios from 'axios';
 import { withRouter } from "react-router-dom";
 import Item from './Item';
 
-import images from '../assets/skate-images/darkstar/*.jpg';
-console.log(images);
+import images from '../assets/skate-images/alien_workshop/*.jpg';
+// console.log(images);
 		// <h1>{window.location.pathname}</h1>
-
 class Catalogue extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			itemName: '',
+			curItem: '',
 			nothingFound: '',
-			onError: false
+			onError: false,
+			fetchItems: [],
+			allItems: [],
+			typesOfProducts: []
+			// currentProducts: []
 		};
-
-		this.fetchItems = [];
 
 		// Main
 		this._pageType = window.location.pathname.slice(1);
 
 		// Methods
-		// this.getItems = this.getItems.bind(this);
-		this.findItem = this.findItem.bind(this);
+		// this.clearItems = this.clearItems.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
+		this.handleCheckboxes = this.handleCheckboxes.bind(this);
+		// this.sortProducts = this.sortProducts.bind(this);
+		// this.handleBrand = this.handleBrand.bind(this);
 		// Temporary fields
-		this._quantityOfSkates = this.fetchItems.length;
 
-		this._typesOfItems = [
-			'darkstar',
-			'deathwish',
-			'spitfire'
-		];
+		// Some Types
 		this._typesOfSorts = [
 			'priceUp',
 			'priceDown',
-			'priceQuantityUp',
-			'priceQuantityDown',
+			'quantityUp',
+			'quantityDown'
 		];
 	}
 
-	findItem(val) {
-		let item = this.fetchItems.find(name => name);
-		if (item)
-			return item 
-		else {
-			this.setState({
-				nothingFound: true
-			})
-			return; 
-		}	
+	handleCheckboxes(ev) {
+		let type = this.state.typesOfProducts.find(i => ev.target.id === i.name);
+		type.checked = ev.target.checked;
+
+		this.handleSearch();
 	}
 
-	getItems(type) {		
-		fetch('https://localhost:3000/' + type)
-			.then(chunk => chunk.json())
-			.then(data => {
-				this.fetchItems = data;
+	handleSearch(ev) {
+		var itemName =  ev ? ev.target.value.trim() : " ";
+
+		// this.setState({curItem: itemName})
+		let filtered;
+		let checked = this.state.typesOfProducts.map(pr =>{if (pr.checked) return pr.name}).filter(i => i !== undefined);
+		console.log(checked)
+		let checkboxed = checked.length ? this.state.fetchItems.filter(i => checked.includes(i.product_type)) : this.state.fetchItems;
+
+		if (itemName.length !== 0 && itemName !== ' ') {
+			var react = this;
+			
+			filtered = checkboxed
+				.filter((item) => { return (
+					item.name.toLowerCase().startsWith(itemName.toLowerCase()))
+			});
+
+			if (checked.length)
+				filtered = filtered.filter(item => checked.includes(item.product_type))
+		} else {
+			filtered = checkboxed;
+		}
+		
+		this.setState({ allItems: filtered })
+	}
+
+	// sortProducts(type) {
+	// 	var types = {
+	// 		"priceUp": function(a, b) {return +a.price - +b.price},
+	// 		"priceDown": function(a, b) {return +b.price - +a.price},
+	// 		"quantityUp": function(a, b) { return +a.quantity - +b.quantity},
+	// 		"quantityDown": function(a, b) { return +b.quantity - +a.quantity}
+	// 	};
+
+	// 	let sorted = this.state.fetchItems.sort(item => { return types[type]; });
+	// 	this.setState({allItems: sorted})
+	// }
+
+	componentDidMount() {
+		axios
+			.get('http://localhost:3000/' + this._pageType)
+			.then(data => { 
+				let checkboxes = [...new Set(data.data.data.map(item => item.product_type))].map(item => {
+					return {
+						name: item,
+						checked: false
+					}
+				});
+				this.setState({
+					fetchItems: data.data.data, 
+					allItems: data.data.data,
+					typesOfProducts: checkboxes
+				})
 			})
 			.catch(err => {
 				this.setState({
@@ -78,69 +120,48 @@ class Catalogue extends React.Component {
 							BRANDS:
 						</h1>
 						<ul className="catalogue-brand">
-							{this._typesOfItems.map((type, ind) => {
+							{this.state.typesOfProducts.map((type, ind) => {
 								return (
-									<li>
-										<input type="checkbox" id={type+ind}/>
-										<label key={type + ind} for={type+ind}>{type}</label>
+									<li key={type + ind}>
+										<input type="checkbox" id={type.name} onChange={this.handleCheckboxes}/>
+										<label htmlFor={type.name}>{type.name}</label>
 									</li>
 								); 
 									
+							})}
+						</ul>
+						<h1 className="catalogue-subtitle">
+							SORT BY:
+						</h1>
+						<ul className="sort-button">
+							{this._typesOfSorts.map((type, ind) => {
+								return <li key={type + ind}>{type}</li>
 							})}
 						</ul>
 					</div>	
 					<div className="catalogue-right">
 						<div className="catalogue-right__top">
 							<div className="skate-quantity">
-								Количество Товара: {this._quantityOfSkates}
+								Количество Товара: {this.state.allItems.length}
 							</div>
-							<ul className="sort-button">
-								{this._typesOfSorts.map((type, ind) => {
-									<li key={type + ind}>{type}</li>
-								})}
-							</ul>
 							<label className="search-field" >
 								<div className="search-field__name">Введите название товара:</div>
-								<input type="text" id="search" onChange={this.itemName}/>
+								<input type="text" id="search" onChange={this.handleSearch}/>
 							</label>
 						</div>
-						<ul className="catalogue-items">
-							<Item 
-								src={images[Object.keys(images)[0]]}
-								title="Beauty"
-								info="Some info about this beautiful creature, created by my hands"
-								price="3990$"
-							/>
-							<Item 
-								src={images[Object.keys(images)[1]]}
-								title="Not"
-								info="Some info about this beautiful creature, created by my hands"
-								price="3990$"
-							/>
-							<Item 
-								src={images[Object.keys(images)[0]]}
-								title="Beauty"
-								info="Some info about this beautiful creature, created by my hands"
-								price="3990$"
-							/>
-							<Item 
-								src={images[Object.keys(images)[1]]}
-								title="Not"
-								info="Some info about this beautiful creature, created by my hands"
-								price="3990$"
-							/>
-							<Item 
-								src={images[Object.keys(images)[0]]}
-								title="Beauty"
-								info="Some info about this beautiful creature, created by my hands"
-								price="3990$"
-							/>
-							<Item 
-								src={images[Object.keys(images)[1]]}
-								title="Not"
-								info="Some info about this beautiful creature, created by my hands"
-								price="3990$"
-							/>
+						<ul className="catalogue-items">{
+								this.state.allItems.map((item, ind) => {
+									return (
+									<li key={item.name.slice(1, 5) + ind}>
+										<Item
+											src={item.img}
+											title={item.name}
+											price={item.price}
+											info={item.description}	
+										></Item>
+									</li>);
+								})
+							}
 						</ul>
 					</div>
 				</div>
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+var cors = require('cors');
 const pgp = require('pg-promise')();
 
 const server = express();
 const db = pgp("postgres://diplom_maker:qwerty@localhost:5432/skate_shop");
-// server.use(express.static('pages'));
 
-db.one('SELECT * FROM product');
+server.use(cors())
 
-server.get('/skates', (req, res) => {
-
+server.get('/skates',(req, res) => {
+	db.any("select * from product where page_type = 'skate'")
+		.then(data => res.status(200).json({data: data}))
+		.catch(err => { throw new Error(err) })
 });
 
 server.listen(3000);
+console.log('server is running');
